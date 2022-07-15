88877f538be1bf4fbfa2b561be98e827dd70d4aa
Prateek Rath <prateekrath28@gmail.com>
2022-07-16 01:54:03 +0530

    render alert message

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Alert from './components/layout/Alert';
 
 function App() {
-  return (
+  return (  
     <GithubProvider>
       <AlertProvider>
     <Router >
       <div className="flex flex-col justify-between h-screen text-slate-200">
         <Navbar />
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
+            <main className='container mx-auto px-3 pb-12'>
+              <Alert/>
+              <Routes>
+            <Route exact path='/' element={<Home/>}/>
             <Route path='/about' element={<About/>}/>
             <Route path='/notfound' element={<NotFound/>}/>
             <Route path='/*' element={<NotFound/>}/>
