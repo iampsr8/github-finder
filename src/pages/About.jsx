a93a73e0a7371e01ac5a2ba990f04aeb271443be
Prateek Rath <prateekrath28@gmail.com>
2022-07-15 18:08:26 +0530

    add about

---
+++ src/pages/About.jsx
@@ -0,0 +1,20 @@
+function About() {
+  return (
+      <div>
+          <h1 className="text-6xl mb-4 text-slate-200">Github finder</h1>
+          <p className='mb-4 text-2xl font-light text-slate-200'>
+        A React app to search GitHub profiles and see profile details. Designed by Hassib Moddasser and Developed by Prateek Rath.
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version: <span className='text-white'>1.0.0</span>
+      </p>
+      <p className='text-lg text-gray-400'>
+        Layout By: <a className='text-white' href='https://twitter.com/hassibmoddasser'>
+        Hassib Moddasser
+        </a>
+      </p>
+    </div>
+  )
+}
+
+export default About
