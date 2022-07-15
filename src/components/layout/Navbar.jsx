f0690d4aef9127892c1313317434739178d6d1b5
Prateek Rath <prateekrath28@gmail.com>
2022-07-15 19:27:38 +0530

    fetch user results

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 
 function Navbar({title}) {
   return (
-      <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content text-slate-200'>
+      <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content text-slate-100'>
           <div className="container mx-auto">
               <div className="flex-none px-2 mx-2">
                   <FaGithub className='inline pr-2 text-3xl' />
---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,31 @@
+import { useEffect,useState } from "react"
+function UserResults() {
+    const [users,setUsers]=useState([])
+    const [isLoading,setIsLoading]=useState(true)
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+    
+    const fetchUsers = async () => {
+        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`)
+        const data=await response.json()
+        setUsers(data)
+        setIsLoading(false)
+    }
+    if (!isLoading) {
+        
+        return (
+            <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+                {users.map((user,index) => (
+                    <h3 key={index}>{ user.login}</h3>
+                ))}
+          </div>
+        )
+    } else {
+        return (
+            <h3>Loading...</h3>
+        )
+    }
+}
+
+export default UserResults
